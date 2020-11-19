0ef5ac4d9abcb5e98b1876c53b3322e23b9a325b
anhquoc540 <anhquocnguyen290@gmail.com>
2020-11-19 08:36:44 +0700

    Green

--- src/anh/util/MathUtility.java
+++ src/anh/util/MathUtility.java
@@ -25,11 +25,12 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1; //
         }
+        return getFactorial(n-1);
         //
-        long result = 1;
+       /* long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
-        return result;
+        return result;*/
     }
 }
